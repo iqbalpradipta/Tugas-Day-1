fc34bb9fab5ad21dcab419f6f1fcc8d0946a9442
Iqbal Pradipta <iqbalpradipta2@gmail.com>
2023-11-10 21:16:19 +0700

    fix bug

--- Day4/js/myproject.js
+++ Day4/js/myproject.js
@@ -3,19 +3,19 @@ let dataProj = [];
 function submitProj(event) {
   event.preventDefault();
 
-  let inputNameProj = document.getElementById('inputNameProj').value;
-  let inputstartdate = document.getElementById('inputstartdate').value;
-  let inputenddate = document.getElementById('inputenddate').value;
-  let inputdescription = document.getElementById('inputdescription').value;
-  let inputImage = document.getElementById('inputImage').files;
+  let inputNameProj = document.getElementById("inputNameProj").value;
+  let inputstartdate = document.getElementById("inputstartdate").value;
+  let inputenddate = document.getElementById("inputenddate").value;
+  let inputdescription = document.getElementById("inputdescription").value;
+  let inputImage = document.getElementById("inputImage").files;
 
-  console.log('namaproject', inputNameProj);
-  console.log('start', inputstartdate);
-  console.log('end', inputenddate);
-  console.log('description', inputdescription);
+  console.log("namaproject", inputNameProj);
+  console.log("start", inputstartdate);
+  console.log("end", inputenddate);
+  console.log("description", inputdescription);
 
   inputImage = URL.createObjectURL(inputImage[0]);
-  console.log('image', inputImage);
+  console.log("image", inputImage);
 
   const proj = {
     title: inputNameProj,
@@ -26,28 +26,28 @@ function submitProj(event) {
   };
 
   dataProj.push(proj);
-  console.log('dataProj', dataProj);
+  console.log("dataProj", dataProj);
   renderMyProject();
 }
 
 function renderMyProject() {
-  document.getElementById('contents').innerHTML = '';
+  document.getElementById("contents").innerHTML = "";
   for (let i = 0; i < dataProj.length; i++) {
-    document.getElementById('contents').innerHTML += `
-        <ul style="display: flex; list-style-upper: none; gap: 50px; align-items: center; text-decoration: none;" >
-          <a href="myproject-detail.html">
-            <li class="outer-box">
-                <img class="img" src="${dataProj[i].image}">
-                <h4 class="title">${dataProj[i].title}</h4>
-                <p class="times">durasi: 3 bulan</p>
-                <p class="desc">${dataProj[i].Description}</p>
-                <img src="assets/icon/playstore.png" class="icon" id="playstore">
-                <img src="assets/icon/android.png" class="icon" id="android">
-                <img src="assets/icon/java.png" class="icon" id="java">
-                <button class="btn-list">Edit</button>
-                <button class="btn-list">Delete</button>
-            </li>
-          </a>
-        </ul>`;
+    document.getElementById("contents").innerHTML += `
+    <a href="myproject-detail.html" style="text-decoration: none">
+        <div class="outer-box">
+          <img class="img" src="${dataProj[i].image}" />
+          <h4 class="title">${dataProj[i].title}</h4>
+          <p class="times">durasi: 3 bulan</p>
+          <p class="desc">${dataProj[i].Description}</p>
+          <img src="assets/icon/node-js.png" class="icon" id="nodejs" />
+          <img src="assets/icon/atom.png" class="icon" id="reactjs" />
+          <img src="assets/icon/next.png" class="icon" id="nextjs" />
+          <img src="assets/icon/typescript.png" class="icon" id="typescript" />
+          <button class="btn-list">Edit</button>
+          <button class="btn-list">Delete</button>
+        </div>
+    </a>
+    `;
   }
 }
